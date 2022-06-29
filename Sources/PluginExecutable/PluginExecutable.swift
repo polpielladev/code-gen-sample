86eb3dd57244d660df948c057a6419448c156717
Pol Piella Abadia <hi@polpiella.dev>
2022-06-29 18:34:39 +0100

    Avoids name clashing by providing full path to type

--- Sources/PluginExecutable/PluginExecutable.swift
+++ Sources/PluginExecutable/PluginExecutable.swift
@@ -2,9 +2,8 @@ import SourceKittenFramework
 import ArgumentParser
 import Foundation
 
-struct MatchedType {
-    let kind: String
-    let name: String
+class CurrentPath {
+    var seenTypes = [String]()
 }
 
 @main
@@ -22,47 +21,62 @@ struct PluginExecutable: ParsableCommand {
         // Needed to ensure that sourcekit runs in a single process
         setenv("IN_PROCESS_SOURCEKIT", "YES", 1)
         let structures = try files.map { try Structure(file: File(path: $0)!) }
-        var matchedTypes = [MatchedType]()
+        var matchedTypes = [String]()
         structures.forEach { walkTree(dictionary: $0.dictionary, acc: &matchedTypes) }
+        print(matchedTypes)
         try createOutputFile(withContent: matchedTypes)
     }
     
-    private func walkTree(dictionary: [String: SourceKitRepresentable], acc: inout [MatchedType]) {
-        acc.append(contentsOf: extractTypeNames(withInheritance: protocolName, from: dictionary))
+    private func walkTree(dictionary: [String: SourceKitRepresentable], acc: inout [String], currentPath: CurrentPath? = nil) {
+        guard let substructure = dictionary["key.substructure"] as? [[String: SourceKitRepresentable]] else {
+            let hasMatched = hasMatchedType(withInheritance: protocolName, from: dictionary)
+            
+            if let name = dictionary["key.name"] as? String, let currentPath = currentPath {
+                currentPath.seenTypes.append(name)
+            }
+            
+            if hasMatched, let currentPath = currentPath {
+                acc.append("\(currentPath.seenTypes.joined(separator: "."))")
+            }
         
-        if let array = dictionary["key.substructure"] as? [[String: SourceKitRepresentable]] {
-            array.forEach { innerDict in
-                let extractedTypes = extractTypeNames(withInheritance: protocolName, from: innerDict)
-                let hasMatched = !extractedTypes.isEmpty
-                
-                acc.append(contentsOf: extractedTypes)
-                
-                guard !hasMatched else { return }
-                
-                // Recurse through every single bit...
-                if let substructure = innerDict["key.substructure"] as? [[String: SourceKitRepresentable]] {
-                    substructure.forEach { walkTree(dictionary: $0, acc: &acc) }
-                }
+            return
+        }
+        
+        substructure.forEach { innerDict in
+            // Set if it's the beginning of a path.
+            let toSendCurrentPath = currentPath ?? CurrentPath()
+            if let name = innerDict["key.name"] as? String {
+                toSendCurrentPath.seenTypes.append(name)
+            }
+            
+            let hasMatched = hasMatchedType(withInheritance: protocolName, from: innerDict)
+            
+            if hasMatched, let currentPath = currentPath {
+                acc.append("\(currentPath.seenTypes.joined(separator: "."))")
+                return
+            }
+            
+            // Recurse through every single bit...
+            if let substructure = innerDict["key.substructure"] as? [[String: SourceKitRepresentable]] {
+                substructure.forEach { walkTree(dictionary: $0, acc: &acc, currentPath: toSendCurrentPath) }
             }
         }
     }
     
-    private func extractTypeNames(withInheritance inheritanceName: String, from dict: [String: SourceKitRepresentable]) -> [MatchedType] {
-        guard let inheritedTypes = dict["key.inheritedtypes"] as? [[String: String]],
-              let name = dict["key.name"] as? String,
-              let kind = dict["key.kind"] as? String else { return [] }
+    private func hasMatchedType(withInheritance inheritanceName: String, from dict: [String: SourceKitRepresentable]) -> Bool {
+        guard let inheritedTypes = dict["key.inheritedtypes"] as? [[String: String]] else { return false }
         
-        return inheritedTypes
+        return !inheritedTypes
             .compactMap { $0["key.name"] }
             .filter { $0 == inheritanceName }
-            .map { _ in MatchedType(kind: kind, name: name) }
+            .isEmpty
     }
     
-    private func createOutputFile(withContent matchedTypes: [MatchedType]) throws {
+    private func createOutputFile(withContent matchedTypes: [String]) throws {
         let testMethods = matchedTypes.map {
             """
             \tfunc testThis() {
-            \t\t\($0.name)
+            \t\t\($0)
             \t}
             """
         }.joined(separator: "\n")
